f8f447f2f99d97b319ec964b013ac2ea4f2d13db
NI5B35 <NI5B35@NI5B09>
2020-07-29 00:17:51 -0400

    first commit

---
+++ ejemploloParaLOsCabros/src/test/java/cl/nacho/EjemploloParaLOsCabrosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.nacho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploloParaLOsCabrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
